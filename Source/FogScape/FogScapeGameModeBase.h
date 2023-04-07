122e7e5ea191f68c579290f909bf8689b599b746
Korso13 <101480776+Korso13@users.noreply.github.com>
2023-04-07 13:56:31 +0400

    Initial commit

---
+++ Source/FogScape/FogScapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FogScapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FOGSCAPE_API AFogScapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
